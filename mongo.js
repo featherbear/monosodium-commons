3773050bb78e4454c5a0096a17ee96d4cc828bf4
Andrew Wong <andrew.j.wong@outlook.com>
2021-04-17 03:55:06 +1000

    Add Mongo provider

---
+++ mongo.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const Schemas = require('./schemas')
+
+const {
+  MONGO_ADDRESS,
+  MONGO_DATABASE,
+  MONGO_USE_AUTH,
+  MONGO_USERNAME,
+  MONGO_PASSWORD
+} = process.env
+
+console.log(MONGO_ADDRESS)
+module.exports = {
+  Models: {
+    Message: mongoose.model('messages', Schemas.MessageSchema),
+    Thread: mongoose.model('threads', Schemas.ThreadSchema),
+    User: mongoose.model('users', Schemas.UserSchema),
+    UserThread: mongoose.model('userThreads', Schemas.UserThreadSchema)
+  },
+  doConnect () {
+    mongoose.connect(
+      `mongodb://${
+        MONGO_USE_AUTH
+          ? `${encodeURIComponent(MONGO_USERNAME)}:${encodeURIComponent(
+              MONGO_PASSWORD
+            )}@`
+          : ''
+      }${MONGO_ADDRESS}/monosodium`,
+      {
+        authSource: 'admin',
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+      }
+    )
+  }
+}
